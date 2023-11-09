2e69f2399e9b482d8871eff71ec37f2b7135b194
Jérôme Commaret <jcommaret@gmail.com>
2023-11-09 19:46:28 +0100

    React : class to className

--- src/components/FonctSection/index.jsx
+++ src/components/FonctSection/index.jsx
@@ -1,31 +1,31 @@
 function FonctSection() {
   return (
-    <section class="function-section">
-      <h2 class="function-section__heading">Fonctionnement</h2>
+    <section className="function-section">
+      <h2 className="function-section__heading">Fonctionnement</h2>
 
-      <div class="function-steps step-animation">
-        <div class="button__function">
-          <div class="button__function__number">
+      <div className="function-steps step-animation">
+        <div className="button__function">
+          <div className="button__function__number">
             <span>1</span>
           </div>
-          <i class="fa-solid fa-mobile"></i>
-          <div class="step__text">Choisissez votre restaurant</div>
+          <i className="fa-solid fa-mobile"></i>
+          <div className="step__text">Choisissez votre restaurant</div>
         </div>
 
-        <div class="button__function">
-          <div class="button__function__number">
+        <div className="button__function">
+          <div className="button__function__number">
             <span>2</span>
           </div>
-          <i class="fa-solid fa-bars"></i>
-          <div class="function_text">Composez votre menu</div>
+          <i className="fa-solid fa-bars"></i>
+          <div className="function_text">Composez votre menu</div>
         </div>
 
-        <div class="button__function">
-          <div class="button__function__number">
+        <div className="button__function">
+          <div className="button__function__number">
             <span>3</span>
           </div>
-          <i class="fa-solid fa-store"></i>
-          <div class="function_text">Dégustez au restaurant</div>
+          <i className="fa-solid fa-store"></i>
+          <div className="function_text">Dégustez au restaurant</div>
         </div>
       </div>
     </section>
