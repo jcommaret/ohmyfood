e2f2512204d0574b2040650b2ba852a4377c1ba2
Jérôme Commaret <jcommaret@gmail.com>
2023-11-11 17:27:43 +0100

    Fancy 404 à construire Fixes #6

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,8 @@
 import { useRouteError } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -16,13 +16,13 @@ export default function ErrorPage() {
       </HelmetProvider>
 
       <Header isNotHomepage={true} />
-
       <div className="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
+        <div className="title-wrapper">
+          <h1 className="sweet-title">
+            <span data-text={error.status}>{error.status}</span>
+          </h1>
+          <span className="top-title">The page doesn't exist</span>
+        </div>
       </div>
 
       <Footer />
